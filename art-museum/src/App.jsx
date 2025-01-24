13247ed50a8cbdf5800b798415569b25179d0213
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2025-01-23 19:23:47 -0800

    done w phase 1

---
+++ art-museum/src/App.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import harvardArt from './data/harvardArt';
+
+console.log(harvardArt);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:
+    <>
+      <h2>Hardvard Art Museum</h2>
+      <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
+    </>
+  },
+  {
+    path: '*',
+    element: <h2>Page Not Found</h2>
+  }
+])
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
