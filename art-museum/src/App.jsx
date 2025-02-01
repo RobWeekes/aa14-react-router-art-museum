57cc2f50e597645bf0993d62c3561160b950b218
Robert Weekes <weekes.robert@gmail.com>
2025-01-31 20:13:35 -0800

    starting ph 3

--- art-museum/src/App.jsx
+++ art-museum/src/App.jsx
@@ -1,31 +1,48 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import harvardArt from './data/harvardArt';
 import GalleryNavigation from './components/GalleryNavigation';
 
 // console.log('harvardArt:', harvardArt);
 
+function Layout() {
+  return (
+    <div className='page-wrapper'>
+      <GalleryNavigation galleries={harvardArt.records} />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
 const router = createBrowserRouter([
   {
-    path: '/',
-    element:
-      <>
-        <h2>Hardvard Art Museum</h2>
-        <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
-        <GalleryNavigation galleries={harvardArt.records} />
-      </>
-  },
-  {
-    path: 'galleries',
-    element: <GalleryNavigation galleries={harvardArt.records} />
-  },
-  {
-    path: '*',
-    element: <h2>Page Not Found</h2>
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element:
+          <>
+            <h2>Harvard Art Museum</h2>
+            <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
+          </>
+      },
+      {
+        path: '*',
+        element: <h2>Page Not Found</h2>
+      }
+    ]
   }
-])
+]);
+  // { REPLACED IN PHASE 3 w LAYOUT
+  //   path: 'galleries',
+  //   element: <GalleryNavigation galleries={harvardArt.records} />
+  // },
+// ])
 
 function App() {
   return <RouterProvider router={router} />;
 }
 
 export default App;
+// export Layout;
